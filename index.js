6f5189a9477dff8c1c21bd734137775a2799d720
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-01 00:31:59 -0800

    Last changes

--- index.js
+++ index.js
@@ -55,27 +55,18 @@ class Withdrawal extends Transaction {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-// DRIVER CODE (yes, keep everything in one file for now... b/c cog load)
-const myAccount = new Account("Master");
+const myAccount = new Account("snow-patrol");
 
-console.log('Starting Account Balance: ', myAccount.balance);
+t1 = new Withdrawal(50.25, myAccount);
+t1.commit();
+console.log('Transaction 1:', t1);
 
-console.log('Attempting to withdraw even $1 should fail...');
-const t1 = new Withdrawal(1.00, myAccount);
-console.log('Commit result:', t1.commit());
-console.log('Account Balance: ', myAccount.balance);
+t2 = new Withdrawal(9.99, myAccount);
+t2.commit();
+console.log('Transaction 2:', t2);
 
-console.log('Depositing should succeed...');
-const t2 = new Deposit(9.99, myAccount);
-console.log('Commit result:', t2.commit());
-console.log('Account Balance: ', myAccount.balance);
+console.log('Balance:', balance);
 
-console.log('Withdrawal for 9.99 should be allowed...');
-const t3 = new Withdrawal(9.99, myAccount);
-console.log('Commit result:', t3.commit());
-
-console.log('Ending Account Balance: ', myAccount.balance);
-console.log("Lookings like I'm broke again");
-
-console.log('Account Transaction History: ', myAccount.transactions);
-
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
